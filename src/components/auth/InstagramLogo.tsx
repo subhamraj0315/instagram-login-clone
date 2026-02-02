bbe771c6973d202fc54a2e77aebf775da3f764e3
subhamraj0315 <subhamraj0315@gmail.com>
2026-02-03 01:08:06 +0530

    Update logo and fix CSS

--- src/components/auth/InstagramLogo.tsx
+++ src/components/auth/InstagramLogo.tsx
@@ -2,10 +2,12 @@ const InstagramLogo = () => {
   return (
     <h1 className="instagram-logo text-center mb-8 select-none">
       <img 
-        src="https://www.instagram.com/static/images/web/logged_out_wordmark.png/7a252de00b20.png"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1200px-Instagram_logo.svg.png?20160616034027"
         alt="Instagram"
         className="h-[51px] mx-auto"
       />
+     
+      
     </h1>
   );
 };
